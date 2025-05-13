e7fee1c7e56fef0c67d365cf64bb1f65f9d5d898
karlijnhuissen11 <lijnohuissen@gmail.com>
2025-05-13 13:28:50 +0200

    points

--- lab5/main.js
+++ lab5/main.js
@@ -129,6 +129,35 @@ let baseLayers = {
 };
 let toc = L.control.layers(baseLayers).addTo(map);
 
+function registerPopUpForInsert(mapInstance) {
+  var popup = L.popup();
+
+  function onMapClick(e) {
+      var lng = e.latlng.lng;
+      var lat = e.latlng.lat;
+
+      var js_function = ''
+      + ' var poi_name = document.getElementById(\'poi_name\').value ; '
+      + ' var reported_by = document.getElementById(\'reported_by\').value ; '
+      + ' insertWFS(' + lng + ',' + lat + ', poi_name, reported_by) ; ';
+
+      var popupContent = ''
+      + '<label for="poi_name">Point of Interest: </label><br>'
+      + '<input type="text" id="poi_name" name="poi_name" value=""><br>'
+      + '<label for="reported_by" >Reported by: </label><br>'
+      + '<input type="text" id="reported_by" name="reported_by" value=""><br>'
+      + '<button type="button" onclick="' + js_function + '">Insert point</button>';
+
+      popup
+      .setLatLng(e.latlng)
+      .setContent(popupContent)
+      .openOn(mapInstance);
+  }
+
+  mapInstance.on('click', onMapClick);
+}
+registerPopUpForInsert(map)
+
 // Register alles
 registerGeoLocate(map);
 register_geocoder(map);
